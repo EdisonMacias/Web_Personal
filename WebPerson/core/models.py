c4297535bbebb890985e3daa38723dd89c49454e
Edison Macias <edisonmacias2012@gmail.com>
2022-06-18 15:41:36 -0500

    Proyecto añadido WebPerson

---
+++ WebPerson/core/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+from django.forms import URLField
+
+# Create your models here.
+
---
+++ WebPerson/core/views.py
@@ -0,0 +1,14 @@
+from cgitb import html
+from django.shortcuts import render,HttpResponse
+
+html_base = """
+    
+"""
+
+# Create your views here.
+def home(request):
+    return render(request, "core/home.html")
+def about(request):
+    return render(request, "core/about.html")
+def contact(request):
+    return render(request, "core/contact.html")
